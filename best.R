f0b1cd6fa0cd2f295133798552bf27fd7872d365
Hope Foster-Reyes <hopealso@gmail.com>
2015-10-03 13:49:02 -0500

    Filter df to state.

--- best.R
+++ best.R
@@ -9,17 +9,23 @@ best <- function(state, outcome) {
     ##  'heart attack', 'heart failure', or 'pneumonia', each
     ##  representing column name roots in the source data file.
     
-    ## ---- Validation ----
+    # ---- Validation ----
     
     
-    ## ---- Processing ----
+    # ---- Processing ----
+    
+    # Turn outcome input into relevant column name 
     x <- strsplit(outcome," ")[[1]]
     Outcome.Title <- paste(toupper(substring(x,1,1)), substring(x,2), sep="", collapse = ".")
     
     Mort.Rate.Name <- paste("Hospital.30.Day.Death..Mortality..Rates.from.",Outcome.Title,sep = "")
-
+    
+    # Read file and clean strings in relevant column, converting to NA values
     rates <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
     rates[,Mort.Rate.Name] <- sub("Not Available",NA,rates[,Mort.Rate.Name])
     rates[,Mort.Rate.Name] <- as.numeric(rates[,Mort.Rate.Name])
-
+    
+    # Filter data frame to state
+    rates <- subset(rates,State==toupper(state),c("Hospital.Name",State,Mort.Rate.Name))
+    # browser()
 }
