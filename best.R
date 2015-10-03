92aa2e52431e9d834a92018639a601bfeb9660f6
Hope Foster-Reyes <hopealso@gmail.com>
2015-10-03 17:14:18 -0500

    Added validation

--- best.R
+++ best.R
@@ -9,29 +9,43 @@ best <- function(state, outcome) {
     ##  'heart attack', 'heart failure', or 'pneumonia', each
     ##  representing column name roots in the source data file.
     
-    # ---- Validation ----
+    
+    # Validate outcome
+    if (!(outcome %in% c("heart attack", "heart failure", "pneumonia"))) {
+        stop("invalid outcome")
+    }
     
     
-    # ---- Processing ----
+    # ---- Import ----
     
     # Turn outcome input into relevant column name 
     x <- strsplit(outcome," ")[[1]]
     Outcome.Title <- paste(toupper(substring(x,1,1)), substring(x,2), sep="", collapse = ".")
     
-    Mort.Rate.Name <- paste("Hospital.30.Day.Death..Mortality..Rates.from.",Outcome.Title,sep = "")
+    Mort.Rate.Name <- paste("Hospital.30.Day.Death..Mortality..Rates.from.", Outcome.Title, sep = "")
     
     # Read file and clean strings in relevant column, converting to NA values
     rates <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
-    rates[,Mort.Rate.Name] <- sub("Not Available",NA,rates[,Mort.Rate.Name])
+    #rates <- read.csv("outcome-of-care-measures.csv")
+    rates[,Mort.Rate.Name] <- sub("Not Available", NA, rates[,Mort.Rate.Name])
     rates[,Mort.Rate.Name] <- as.numeric(rates[,Mort.Rate.Name])
-    
+
+    # browser()
+        
+    # Validate state
+    if (!(state %in% rates$State)) {
+        stop("invalid state")
+    }
+
+
+    # ---- Processing ----
+
     # Filter data frame to state
-    rates <- subset(rates,State==toupper(state),c("Hospital.Name", "State", Mort.Rate.Name))
-    
+    rates <- subset(rates, State == state, c("Hospital.Name", "State", Mort.Rate.Name))
     
     # Sort by Mortality Rate, Hospital Name
-    rates <- rates[order(rates[[Mort.Rate.Name]],rates[["Hospital.Name"]]),]
+    rates <- rates[order(rates[[Mort.Rate.Name]], rates[["Hospital.Name"]]), ]
     
-    # browser()
+    #return(as.character(rates[1,1]))
     return(rates[1,1])
 }
