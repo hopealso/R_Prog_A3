aa564b1d4b5921d1308d429a0158f9be0317716c
Hope Foster-Reyes <hopealso@gmail.com>
2015-10-04 08:33:07 -0500

    Import subset of columns

--- rankall.R
+++ rankall.R
@@ -4,4 +4,32 @@ rankall <- function(outcome, num = "best") {
     ## For each state, find the hospital of the given rank
     ## Return a data frame with the hospital names and the
     ## (abbreviated) state name
+    
+    # Validate outcome
+    if (!(outcome %in% c("heart attack", "heart failure", "pneumonia"))) {
+        stop("invalid outcome")
+    }
+    
+    # ---- Import ----
+    
+    # Turn outcome input into relevant column name 
+    x <- strsplit(outcome," ")[[1]]
+    Outcome.Title <- paste(toupper(substring(x,1,1)), substring(x,2), sep="", collapse = ".")
+    
+    Mort.Rate.Name <- paste("Hospital.30.Day.Death..Mortality..Rates.from.", Outcome.Title, sep = "")
+    
+    # Specify data types and columns to skip
+    Spec <- c(rep("character", 2), rep("NULL", 4), "factor", rep("NULL", 3),
+              rep(c("character", rep("NULL", 5)),3), rep("NULL", 6*3))
+
+    browser()
+    
+    # Read file and clean strings in relevant column, converting to NA values
+    #rates <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
+    rates <- read.csv("outcome-of-care-measures.csv", colClasses = Spec)
+    #rates <- read.csv("outcome-of-care-measures.csv")
+    rates[,Mort.Rate.Name] <- sub("Not Available", NA, rates[,Mort.Rate.Name])
+    rates[,Mort.Rate.Name] <- as.numeric(rates[,Mort.Rate.Name])
+    
+
 }
